793647e1963a60a726cea5b52d826846d49d62cc
Efrain Diaz <efraindiazcode@gmail.com>
2017-05-31 14:45:33 -0500

    Bloqueo de pantalla usando el sensor de proximidad

--- app/src/main/java/com/example/android/sensorapp1/MainActivity.java
+++ app/src/main/java/com/example/android/sensorapp1/MainActivity.java
@@ -1,21 +1,37 @@
 package com.example.android.sensorapp1;
 
+import android.app.KeyguardManager;
+import android.app.admin.DeviceAdminReceiver;
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.PowerManager;
 import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+    DevicePolicyManager dpm;
+
+    private PowerManager mPowerManager;
+    private PowerManager.WakeLock mWakeLock;
 
     private TextView sensorText;
     private Sensor sensor;
     private SensorManager sensorManager;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +43,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
 
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL);
+
     }
 
     @Override
@@ -35,19 +52,41 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         sensorText.setText("Distancia: " + distance);
         if(distance == 0){
 
-
-            Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 0);
+                //screen is not locked
+                dpm = (DevicePolicyManager) getSystemService(Context.DEVICE_POLICY_SERVICE);
+                try {
+                    dpm.lockNow();
+                    finish();
+                } catch (SecurityException e) {
+                    Intent it = new Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN);
+                    it.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, new ComponentName(this, DeviceAdminReceiver.class));
+                    startActivityForResult(it, 0);
+                }
+            //Reduce el brillo de la pantalla a 0
+            //Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 0);
 
         }
+        /*
         else if(distance == 1){
 
-
-            Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 255);
-        }
+            //Aumenta el brillo de la pantalla
+            //Settings.System.putInt(getContentResolver(), Settings.System.SCREEN_BRIGHTNESS, 255);
+        }*/
     }
 
     @Override
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (resultCode == RESULT_OK) {
+            dpm.lockNow();
+        } else {
+            Toast.makeText(this, "Se necestan permisos de administrador", Toast.LENGTH_LONG).show();
+        }
+        finish();
     }
 }
